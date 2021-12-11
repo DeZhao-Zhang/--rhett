19b9545dbaf83ffe229f0c94ea5b23b525357aea
Rhett <jhsyzxy@gmail.com>
2021-12-11 17:22:47 +0800

    1.新增关于active object的尝试 但是以脚本的方式写起来 存在很多问题  2.新增拙劣设计的症状和一些基本的设计原则笔记

---
+++ ACTIVE OBJECT/SleepCommand.java
@@ -0,0 +1,10 @@
+import ActivateObjectEngine;
+import Command;
+
+
+public class SleepCommand implements Command{
+    private Command wakeupCommand = null;
+    private ActivateObjectEngine engine = null;
+    private long sleepTime = 0;
+    // 按照脚本的方式写起来 疑问多多啊
+}
